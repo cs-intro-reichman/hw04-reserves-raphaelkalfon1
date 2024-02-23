346b38791620b106e79f2c4977903ec20c67ba34
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-02-23 15:51:58 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -1,42 +1,80 @@
 public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
+
     public static void main(String[] args) {
         
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest(String string) {
+        String result = ""; 
+    
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            int value = (int) ch;
+    
+            if ("aeiou".indexOf(ch) >= 0) { 
+                value -= 32; 
+                result += (char) value;
+            } else if ("AEIOU".indexOf(ch) >= 0) { 
+                result += ch;
+            } else {
+                result += ch;
+            }
+        }
+    
+        return result;
     }
+    
+        
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        String result = "";
+        boolean newWord = false; 
+        boolean firstWord = true; 
+
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch == ' ') { 
+                newWord = true;
+            } else {
+                int Value = (int) ch;
+                if (newWord || i == 0) { 
+                    if (firstWord) { 
+                        result += (Value >= 65 && Value <= 90) ? (char)(Value + 32) : ch; 
+                        firstWord = false; 
+                    } else { 
+                        result += (Value >= 97 && Value <= 122) ? (char)(Value - 32) : ch; 
+                    }
+                    newWord = false;
+                } else { 
+                    result += (Value >= 65 && Value <= 90) ? (char)(Value + 32) : ch; 
+                }
+            }
+        }
+    
+        return result;
     }
+    
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static int[] allIndexOf(String string, char chr) {
+
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                count++;
+            }
+        }
+    
+        int[] indices = new int[count]; 
+        int index = 0; 
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                indices[index] = i; 
+                index++; 
+            }
+        }
+    
+        return indices;
     }
 }
+    
